c077881948a08b603c9bc098d332392a2ad8fc44
Prajwal Deval <prajwalxdeval@gmail.com>
2022-03-28 09:56:54 +0200

    changes and shit

--- defs.py
+++ defs.py
@@ -1,6 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+R = 0.7 #m
+r_hub = 0.25*R
+b_col = 46 #deg
+U0 = 60 #m/s
+RPM = 1200
+omega=RPM*2*np.pi/60 #rad/s
+h = 2000 #m
+nb = 3 #number of blades
+n = 50 #number of discretised blade elements
+rho = 1.00649 #kg/m3 at 2000m altitude (ISA)
+
 def chord(mu):
     return 0.18 - 0.06*mu
 
@@ -30,19 +41,20 @@ def aero_coeffs(alpha,filename = 'ARAD8pct_polar.txt'):
     cd = np.interp(alpha,data[:,0].data[:,2])
     return cl, cd
 
-
 def BE_loads(a,ap,r,dr,b,c):
     Vtan = RPM*r*(1+ap)
     Vax = U0*(1+a) #1-a
     Vps= Vtan**2+Vax**2
     phi = np.atan(Vax,Vtan)
-    alpha = phi - b
+    b = b + b_col
+    alpha = - phi + b
     cl, cd = aero_coeffs(alpha)
     L = 0.5*c*rho*Vps*cl
     D = 0.5*c*rho*Vps*cd
     Faz = L*np.sin(phi) - D*np.cos(phi)
     Fax = L*np.cos(phi) + D*np.sin(phi)
-    return Fax,Faz
+    gamma = 0.5 * np.sqrt(Vps) * cl * c  # vorticity
+    return Vax,Vtan,Fax,Faz,gamma
 
 def MT_induction(Fax,Faz,r,dr,b,c,Glauert,Prandtl):
     CT = (Fax*nb*r)/(0.5*rho*U0**2*2*np.pi*r*dr)
@@ -68,7 +80,32 @@ def MT_induction(Fax,Faz,r,dr,b,c,Glauert,Prandtl):
 
     return a,ap
 
+def solve_anul(Uinf, r, dr, c, b):
 
+    #initialization of variables
+    a = 0   # axial induction factor
+    ap = 0  # tangential induction factor
+
+    convergence_error = 0.00001
+    max_nr_iterations = 100
+
+    for i in range(max_nr_iterations):
+
+        Vax, Vtan, Fax, Faz, gamma = BE_loads(a,ap,r,dr,b,c)
+        an, apn = MT_induction(Fax,Faz,r,dr,b,c, Prandtl=True, Glauert=True)
+
+        anext = 0.25*a + 0.75*an
+        apnext = 0.25*ap + 0.75*apn
+
+        if (np.abs(a-anext)<convergence_error):
+            print(i)
+            break
+        if i==max_nr_iterations:
+            print('not conveerged')
+        a = anext
+        ap = apnext
+
+    return a,ap,Fax,Faz,gamma
 
 
 if __name__=='__main__':
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from defs import *
 
+r_cos,dr_cos = geometry_cosine()
+r_con,dr_con = geometry_constant()
+
+c = chord(r_cos/R)
+b = twist(r_cos/R)
+
+
+for i in range(len(r_cos)):
+    a,ap,Fax,Faz,gamma = solve_anul(U0,r_cos[i],dr_cos[i],c[i],b[i])
